004a262b194e0ca8396c802537c8f415a770b841
Michael Dojčár <michaeldojcar@gmail.com>
2020-07-07 14:21:00 +0200

    sidebar admin link

--- resources/views/user/layout/sidebar.blade.php
+++ resources/views/user/layout/sidebar.blade.php
@@ -14,6 +14,24 @@
             Teamy
         </a>
     </li>
+
+    @if(Auth::user()->is_admin)
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Konfigurace</span>
+            <a class="d-flex align-items-center text-muted"
+               href="#">
+                <span data-feather="plus-circle"></span>
+            </a>
+        </h6>
+
+        <li class="nav-item">
+            <a class="nav-link {{ request()->is('organizace-akce/team/*/clenove') ? 'active' : '' }}"
+               href="{{route('admin.dashboard')}}">
+                <span data-feather="users"></span>
+                Administrace
+            </a>
+        </li>
+    @endif
 </ul>
 
 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
