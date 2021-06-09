a2c352db2412cbd96e437c85fa8c4a25d555b25f
Michael Dojčár <michaeldojcar@gmail.com>
2021-06-10 00:29:17 +0200

    object data storage experiment

---
+++ app/Http/Controllers/AttributeRelationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AttributeRelation;
+use Illuminate\Http\Request;
+
+class AttributeRelationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AttributeRelation  $attributeRelation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AttributeRelation $attributeRelation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AttributeRelation  $attributeRelation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AttributeRelation $attributeRelation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AttributeRelation  $attributeRelation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AttributeRelation $attributeRelation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AttributeRelation  $attributeRelation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AttributeRelation $attributeRelation)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ObAttributeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ObAttribute;
+use Illuminate\Http\Request;
+
+class ObAttributeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ObAttribute  $obAttribute
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ObAttribute $obAttribute)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ObAttribute  $obAttribute
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ObAttribute $obAttribute)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ObAttribute  $obAttribute
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ObAttribute $obAttribute)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ObAttribute  $obAttribute
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ObAttribute $obAttribute)
+    {
+        //
+    }
+}
